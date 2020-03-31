a1360bebf16707ad53fa0a04d5828f01c68d0d56
KotR9001 <57807780+KotR9001@users.noreply.github.com>
2020-03-30 19:49:21 -0500

    Add webpage.

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -1,50 +1,7 @@
-﻿CREATE TABLE "departments" (
-    "dept_no" varchar(4)   NOT NULL,
-    "dept_name" varchar(20)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" varchar(4)   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
-);
 
-CREATE TABLE "dept_manager" (
-    "dept_no" varchar(4)   NOT NULL,
-    "emp_no" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
-);
-
-CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar(20)   NOT NULL,
-    "last_name" varchar(20)   NOT NULL,
-    "gender" varchar(1)   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
-);
-
-CREATE TABLE "titles" (
-    "emp_no" int   NOT NULL,
-    "title" varchar(30)   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
-);
 CREATE TABLE "departments" (
     "dept_no" varchar(4)   NOT NULL,
     "dept_name" varchar(20)   NOT NULL,
